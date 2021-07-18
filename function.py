b58eafb535e8a824b6e5f10e4a980db9d27b93d9
pushpa pnwar <pushpa55panwar@gmail.com>
2021-07-18 12:05:24 +0530

    function program

---
+++ function.py
@@ -0,0 +1,16 @@
+# function 
+def sayhii(name='same'):
+  sayhii('beth')
+# return value
+def  getsum(num1,num2):
+ total=num1+num2
+ return total
+numsum=getsum(2,3)
+print(numsum)
+
+def addonetonum(num):
+ num=+1
+ return num
+num=5
+new_num=addonetonum(num)
+print(new_num)
---
+++ lambda.py
@@ -0,0 +1,7 @@
+#lambda function
+getsum=lambda num1
+num2:num1+num2
+print(getsum(9,2))
+addonetonum=lambda num:
+num+1
+print(addonetonum(5))
